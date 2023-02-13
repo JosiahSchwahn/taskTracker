980873cdd5abd4e6698d05be62703b076863097d
Josiah Schwahn <josiah@Josiahs-MacBook-Pro.local>
2023-02-13 13:23:33 -0700

    modal page is working ~ working on local storage implementation

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,10 +3,24 @@
 const addTaskBtn = document.querySelector("button.add-task-button");
 const modalPage = document.querySelector(".modal-page")
 const closeModal = document.querySelector(".modal-close-button")
+const modalExitButton = document.querySelector("button.exit-button")
+
+taskItemList = {}
+
 
 addTaskBtn.addEventListener(`click`, function(){
-        
+    alert("showing modal");
+    modalPage.classList.add('show');
 });
+
+[closeModal,modalExitButton].forEach(function(element) {
+    element.addEventListener("click", function() {
+        modalPage.classList.remove('show');
+    });
+});
+
+
+
 
 
 
