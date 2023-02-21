9618de5a107a2237ed8c058d73ebe15123df49d2
Josiah Schwahn <josiah@Josiahs-MacBook-Pro.local>
2023-02-21 12:17:59 +0000

    javascript task objects and submitForm eventListener done, working on passing form data to JSON object

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,12 +4,11 @@ const addTaskBtn = document.querySelector("button.add-task-button");
 const modalPage = document.querySelector(".modal-page")
 const closeModal = document.querySelector(".modal-close-button")
 const modalExitButton = document.querySelector("button.exit-button")
-
-taskItemList = {}
+const formInput = document.querySelector(".task-form")
 
 
 addTaskBtn.addEventListener(`click`, function(){
-    alert("showing modal");
+   
     modalPage.classList.add('show');
 });
 
@@ -18,6 +17,65 @@ addTaskBtn.addEventListener(`click`, function(){
     element.addEventListener("click", function() {
         modalPage.classList.remove('show');
     });
+});
+
+/* TO DO 
+
+    - constructor for task objects
+    - Store objects in an array // locally
+    - Add / Delete / Edit Tasks
+
+    - User email/password authentication
+    - MongoDB document learning to store tasks in DB documents
+*/
+
+/* Task Displays */
+
+let taskItems = []
+
+/*Enum for category types */
+const categoryTypes = {
+    health: "Health",
+    rest: "Rest",
+    work: "Work",
+    community: "Community",
+    discovery: 'Discovery'
+}
+
+
+function Task(){
+    this.name = "";
+    this.category = categoryTypes[0];
+    this.weekFrequency = 0;
+    this.completedToday = false;
+    this.favorite = false;
+}
+
+
+/* form eventListener */
+
+formInput.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const formData = new FormData(formInput);
+    let taskItem = new Task();
+    console.log(typeof formData);
+   
+    for(data of formData){
+        console.log(data);
+    }
+
+
+/*
+    const newData = [];
+
+    realData.forEach((val) => {
+    let obj = {};
+    val.forEach(v => obj[v.key] = v.value);
+    newData.push(obj);
+    });
+*/
+
+    
 });
 
 
