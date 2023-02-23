8a4aa006b2e6384b15086e08c2abfbfc882c2ad3
Josiah Schwahn <josiah@Josiahs-MacBook-Pro.local>
2023-02-23 20:16:11 +0000

    task adding is working with name, need to finish category type, days per week, setting container, etc.... ~ will need to start adding functions / refactoring

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,11 @@
-/* Query selectors ES6 */
-
+/* Global Query selectors ES6 on window load*/
 const addTaskBtn = document.querySelector("button.add-task-button");
 const modalPage = document.querySelector(".modal-page")
 const closeModal = document.querySelector(".modal-close-button")
 const modalExitButton = document.querySelector("button.exit-button")
 const formInput = document.querySelector(".task-form")
+const taskItemContainer = document.querySelector(".task-items")
+
 
 /* Task Displays - global scope */
 var taskItems = []
@@ -18,7 +19,6 @@ const categoryTypes = {
     discovery: 'Discovery'
 }
 /*object constructor for taskItems */
-
 class Task{
     constructor(){
         this.name = undefined
@@ -28,7 +28,6 @@ class Task{
         this.favorite = false;
     }
 }
-
 
 addTaskBtn.addEventListener(`click`, function(){
     modalPage.classList.add('show');
@@ -40,12 +39,13 @@ addTaskBtn.addEventListener(`click`, function(){
     });
 });
 
-
 formInput.addEventListener('submit', (event) =>{
     event.preventDefault();
     const formData = new FormData(formInput);
     const taskItem = new Task();
     formData.forEach((value, key) => {
+        /* checks to see what values need to be mapped to a number,
+        all others are put into object as a string */
         if(key === "weekFrequency"){
             taskItem[key] = parseInt(value);
         } else{        /*console.log(key);*/
@@ -55,9 +55,36 @@ formInput.addEventListener('submit', (event) =>{
     taskItems.push(taskItem);
     console.log(taskItems);
     modalPage.classList.remove('show');
+
+
+    /* creating DOM structure for a taskItem */
+    const newTaskContainer = document.createElement("div");
+    newTaskContainer.classList.add("task-item-containers");
+
+    const taskName = document.createElement("div");
+    taskName.classList.add("task-name")
+    taskName.textContent = taskItem.name;
+    newTaskContainer.appendChild(taskName);
+
+
+    const categoryType = document.createElement("div");
+    const daysPerWeek = document.createElement("div");
+    const completedToday = document.createElement("div");
+    const favSettingsContainer = document.createElement("div");
+    
+    taskItemContainer.appendChild(newTaskContainer);
+
+
+    
+
 });
 
-
+// function definition to create a new element
+const creatingMenuItems = (name) => {
+    let liElement = document.createElement("div");
+    divElement.textContent = name;
+    return divElement;
+  };
 
 
 
