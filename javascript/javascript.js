0170693de358d99b2944451ae82f5d7e114109e2
Josiah Schwahn <josiah@Josiahs-MacBook-Pro.local>
2023-03-04 12:22:03 -0700

    simplified app, added svg and task counter

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -46,6 +46,7 @@ addTaskBtn.addEventListener(`click`, function(){
     });
 });
 
+
 formInput.addEventListener('submit', (event) =>{
     event.preventDefault();
     const formData = new FormData(formInput);
@@ -69,57 +70,65 @@ formInput.addEventListener('submit', (event) =>{
 
 });
 
+function updateTextCounter(){
+
+    const taskCounter = document.querySelector(".task-counter")
+    taskCounter.textContent = `Task ${taskItems.length} / 5`;
+
+}
+
+
 function createTaskItem(taskItem){
-
     /* creating DOM structure for a taskItem */
-    const newTaskContainer = document.createElement("div");
-    newTaskContainer.classList.add("task-item-containers");
+    if (taskItems.length > 5){
 
-    const taskName = document.createElement("div");
-    taskName.classList.add("task-name")
-    taskName.textContent = taskItem.name;
-    newTaskContainer.appendChild(taskName);
+        alert("The maximum amount of tasks is 5")
 
+    } else {
 
+        const newTaskContainer = document.createElement("div");
+        newTaskContainer.classList.add("task-item-containers");
 
-    const categoryType = document.createElement("div");
-    categoryType.classList.add("category-type");
-    categoryType.textContent = taskItem.category;
-    newTaskContainer.appendChild(categoryType);
+        const taskName = document.createElement("div");
+        taskName.classList.add("task-name")
+        taskName.textContent = taskItem.name;
+        newTaskContainer.appendChild(taskName);
 
-
-    const daysPerWeek = document.createElement("div");
-    daysPerWeek.classList.add("days-per-week");
-    daysPerWeek.textContent = taskItem.weekFrequency;
-    newTaskContainer.appendChild(daysPerWeek);
+        const categoryType = document.createElement("div");
+        categoryType.classList.add("category-type");
+        categoryType.textContent = taskItem.category;
+        newTaskContainer.appendChild(categoryType);
 
 
-    const completedToday = document.createElement("div");
-    const completedInput = document.createElement("input")
-    const completedLabel = document.createElement("label")
+        const daysPerWeek = document.createElement("div");
+        daysPerWeek.classList.add("days-per-week");
+        daysPerWeek.textContent = taskItem.weekFrequency;
+        newTaskContainer.appendChild(daysPerWeek);
 
 
-    const checkBoxInput = document.createElement("input");
-    const checkBoxLabel = document.createElement("label");
+        const favSettingsContainer = document.createElement("div");
+        favSettingsContainer.classList.add("fav-settings-container")
+        newTaskContainer.appendChild(favSettingsContainer);
+        const svg = createSVG("svg", {width:"14",height:"4", viewbox:"0 0 14 4"});
+        //Create a path in SVG's namespace
+        /* refactor this - adding settings icon to delete and edit task */
+        var svgPath = document.createElementNS("http://www.w3.org/2000/svg", 'path'); 
+        var svgPath2 = document.createElementNS("http://www.w3.org/2000/svg", 'path'); 
+        var svgPath3 = document.createElementNS("http://www.w3.org/2000/svg", 'path'); 
+        svgPath.setAttributeNS(null, "d", "M13.4874 0.512567C12.804 -0.170848 11.696 -0.170848 11.0126 0.512567C10.3291 1.19598 10.3291 2.30402 11.0126 2.98744C11.696 3.67085 12.804 3.67085 13.4874 2.98744C14.1709 2.30405 14.1709 1.19601 13.4874 0.512567Z");
+        svgPath2.setAttributeNS(null, "d", "M8.23744 0.512567C7.55402 -0.170848 6.44598 -0.170848 5.76257 0.512567C5.07915 1.19598 5.07915 2.30402 5.76257 2.98744C6.44598 3.67085 7.55402 3.67085 8.23744 2.98744C8.92085 2.30405 8.92085 1.19601 8.23744 0.512567Z");
+        svgPath3.setAttributeNS(null, "d", "M2.98744 0.512567C2.30402 -0.170848 1.19598 -0.170848 0.512564 0.512567C-0.170852 1.19598 -0.170852 2.30402 0.512564 2.98744C1.19598 3.67085 2.30402 3.67085 2.98744 2.98744C3.67085 2.30405 3.67085 1.19601 2.98744 0.512567Z");
+        svg.appendChild(svgPath);
+        svg.appendChild(svgPath2);
+        svg.appendChild(svgPath3);
+        favSettingsContainer.appendChild(svg);
+
+        /* Updates the task counter */
+        updateTextCounter();
 
 
-
-    const favSettingsContainer = document.createElement("div");
-    favSettingsContainer.classList.add("fav-settings-container")
-    newTaskContainer.appendChild(favSettingsContainer);
-    
-
-
-   
-    const svg = createSVG("svg", {width:"24",height:"24", viewbox:"0 0 24 24", fill: "none"});
-    //Create a path in SVG's namespace
-    favSettingsContainer.appendChild(svg);
-
-
-
-  
-
-    return newTaskContainer;
+        return newTaskContainer;
+    }
 }
 
 
